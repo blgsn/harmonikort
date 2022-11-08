6c274c05de1056a6505385084d593d4d520b112a
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-08 13:58:41 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,14 @@
 
 public class Main {
-
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        double average=0.0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+            
+        }
+        average=sum / numbers.length;
+        System.out.println(1/average);
+    }
 }
